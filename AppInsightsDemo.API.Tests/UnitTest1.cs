53ae79da971fa579a33a1a590c05b543b67ad065
Jason Specland <jaspecla@microsoft.com>
2020-01-28 13:30:03 -0500

    added testing

---
+++ AppInsightsDemo.API.Tests/UnitTest1.cs
@@ -0,0 +1,14 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace AppInsightsDemo.API.Tests
+{
+  [TestClass]
+  public class UnitTest1
+  {
+    [TestMethod]
+    public void ShouldAlwaysPass()
+    {
+      Assert.IsTrue(true);
+    }
+  }
+}
